5a632fee774893b7b8462d744bfdfedaaf35f5e1
Pritom Hazarika <mimikaan@gmail.com>
2019-10-07 16:47:13 +0530

    Update bubbleSort.py

--- languages/python/bubbleSort.py
+++ languages/python/bubbleSort.py
@@ -1,4 +1,6 @@
-times = [66,57,54,53,64,52,59]
+'''Time is a list with numeric value
+    we are sorting the list in this code using bubble sort'''
+time = [66,57,54,53,64,52,59]
 for i in range(len(times)):
     for j in range(len(times)-i-1):
         if times[j] > times[j+1]:
